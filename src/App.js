0f96b9563eb60db29c1f91d1b9d13c89776e47f5
jongjin-j <jjjung3571@gmail.com>
2021-06-16 23:50:40 -0400

    Connected to API CovidActNow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import covidService from './services/covidData'
+import React, { useEffect, useState } from 'react'
+
+const Data = ({p, cases, vax}) => {
+  return(
+    <div>
+      <p>Total population: {p}</p>
+      <p>Active cases: {cases}</p>
+      <p>Number of people vaccinated: {vax}</p>
+    </div>
+  )
+}
+
 
 function App() {
+
+  const baseURL = 'https://api.covidactnow.org/v2/state/'
+  const nextURL = '.json?apiKey=d544d096cb08454ca99717745e489f14'
+
+  const [state, setState] = useState('MA')
+  const [population, setPopulation] = useState(0)
+  const [numOfCases, setnumOfCases] = useState(0)
+  const [numOfVax, setnumOfVax] = useState(0)
+  //const [stateData, setStateData] = useState([])
+
+  useEffect(() => {
+    covidService
+      .getData(baseURL + state + nextURL)
+      .then(response => {
+        //setStateData(response)
+        setPopulation(response.population)
+        setnumOfCases(response.actuals.cases)
+        setnumOfVax(response.actuals.vaccinationsCompleted)
+      })
+  }, [state])
+
+  const handleStateChange = (event) => {
+    setState(event.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className = "App">
+        <h1>US covid</h1>
+        <form>
+          <label>
+            <select value = {state} onChange = {handleStateChange}>
+              <option>MA</option>
+              <option>TX</option>
+            </select>
+          </label>
+        </form>
+      </div>
+      <div>
+        <Data p = {population} cases = {numOfCases} vax = {numOfVax}/>
+      </div>
     </div>
   );
 }
---
+++ src/services/covidData.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const getData = (baseURL) => {
+    const request = axios.get(baseURL)
+    return request.then(response => response.data)
+}
+
+
+export default {getData}
